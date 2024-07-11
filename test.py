861411ecb6367791690419cbd2ec0a49d3c8e4f0
nodiraegamberdiyeva <n.egamberdiyeva017@gmail.com>
2024-07-11 09:10:53 +0500

    finish python practical module

---
+++ test.py
@@ -0,0 +1,35 @@
+import math
+
+'''list = []
+x = -2
+while x<2:
+    y = math.exp(x)
+    el = {'x': x, 'y': y}
+    list.append(el)
+    x=x+0.5
+
+f = open("demofile2.txt", "a")
+f.write(list)
+f.close()
+
+n = int(input('n sonini kiriting: '))
+
+x=1
+s=0
+while x<=n:
+    s = s + 1/x
+    x = x + 1
+
+print(s)
+
+a = int(input('a sonini kiriting: '))
+b = int(input('b sonini kiriting: '))
+alfa = int(input('a va b tomon orasidagi burchak gradusini kiriting: '))
+
+alfa_rad = alfa * math.pi/180
+S = a*b*math.sin(alfa_rad)/2
+print(f'Uchburchak yuzasi: {S}')'''
+if 1<2:
+    print('ture')
+
+
